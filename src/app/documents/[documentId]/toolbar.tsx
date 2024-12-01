611cd3ad38b42876f3c31a228f911dabe9a1a2e6
BobbyLin23 <linzhangsheng23@gmail.com>
2024-12-01 22:02:07 +0800

    feat: add editor tools

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,9 +1,22 @@
 'use client'
 
-import { LucideIcon, Undo2Icon } from 'lucide-react'
+import {
+	BoldIcon,
+	ItalicIcon,
+	ListTodoIcon,
+	LucideIcon,
+	MessageSquarePlusIcon,
+	PrinterIcon,
+	Redo2Icon,
+	RemoveFormattingIcon,
+	SpellCheckIcon,
+	UnderlineIcon,
+	Undo2Icon,
+} from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 import { useEditorStore } from '@/store/use-editor-store'
+import { Separator } from '@/components/ui/separator'
 
 interface ToolbarButtonProps {
 	onClick?: () => void
@@ -44,12 +57,80 @@ export const Toolbar = () => {
 				icon: Undo2Icon,
 				onClick: () => editor?.chain().focus().undo().run(),
 			},
+			{
+				label: 'Redo',
+				icon: Redo2Icon,
+				onClick: () => editor?.chain().focus().redo().run(),
+			},
+			{
+				label: 'Print',
+				icon: PrinterIcon,
+				onClick: () => window.print(),
+			},
+			{
+				label: 'Spell Check',
+				icon: SpellCheckIcon,
+				onClick: () => {
+					const current = editor?.view.dom.getAttribute('spellcheck')
+					editor?.view.dom.setAttribute(
+						'spellcheck',
+						current === 'false' ? 'true' : 'false',
+					)
+				},
+			},
+		],
+		[
+			{
+				label: 'Bold',
+				icon: BoldIcon,
+				onClick: () => editor?.chain().focus().toggleBold().run(),
+				isActive: editor?.isActive('bold'),
+			},
+			{
+				label: 'Italic',
+				icon: ItalicIcon,
+				onClick: () => editor?.chain().focus().toggleItalic().run(),
+				isActive: editor?.isActive('italic'),
+			},
+			{
+				label: 'Underline',
+				icon: UnderlineIcon,
+				onClick: () => editor?.chain().focus().toggleUnderline().run(),
+				isActive: editor?.isActive('underline'),
+			},
+		],
+		[
+			{
+				label: 'Comment',
+				icon: MessageSquarePlusIcon,
+				onClick: () => {},
+				isActive: false,
+			},
+			{
+				label: 'List Todo',
+				icon: ListTodoIcon,
+				onClick: () => editor?.chain().focus().toggleTaskList().run(),
+				isActive: editor?.isActive('taskList'),
+			},
+			{
+				label: 'Remove Formatting',
+				icon: RemoveFormattingIcon,
+				onClick: () => editor?.chain().focus().unsetAllMarks().run(),
+			},
 		],
 	]
 
 	return (
 		<div className="flex min-h-[40px] items-center gap-x-0.5 overflow-x-auto rounded-[24px] bg-[#F1F4F9] px-2.5 py-0.5">
 			{sections[0].map((item) => (
+				<ToolbarButton key={item.label} {...item} />
+			))}
+			<Separator orientation="vertical" className="h-6 bg-neutral-200" />
+			{sections[1].map((item) => (
+				<ToolbarButton key={item.label} {...item} />
+			))}
+			<Separator orientation="vertical" className="h-6 bg-neutral-200" />
+			{sections[2].map((item) => (
 				<ToolbarButton key={item.label} {...item} />
 			))}
 		</div>
