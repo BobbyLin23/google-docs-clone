5d78f2fa6ddb15a26f22af17bdc57a35ee4b48a1
BobbyLin23 <linzhangsheng23@gmail.com>
2024-12-01 21:52:47 +0800

    feat: add editor store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
-  return (
-    <div className="p-2">
-      <p>
-        <Button>Click</Button>
-      </p>
-    </div>
-  )
+	return (
+		<div className="p-2">
+			<p>
+				<Button>Click</Button>
+			</p>
+		</div>
+	)
 }
